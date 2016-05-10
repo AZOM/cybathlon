11151789b97deed5e3c0574982ac9d74ee9f9501
A. Zolliker <andreas.zolliker@hsr.ch>
2016-05-10 10:15:44 +0200

    Remove ActionBar completely by design

--- ZEDControl/app/src/main/java/ch/hsr/zedcontrol/MainActivity.java
+++ ZEDControl/app/src/main/java/ch/hsr/zedcontrol/MainActivity.java
@@ -28,21 +28,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
 
-        removeActionBar();
         removeStatusAndNavigationBar();
     }
 
 
-    private void removeActionBar() {
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.hide();
-        }
-    }
-
-
     private void removeStatusAndNavigationBar() {
-
         // Note that some of these constants are new as of API 16 (Jelly Bean)
         // and API 19 (KitKat). It is safe to use them, as they are inlined
         // at compile-time and do nothing on earlier devices.
