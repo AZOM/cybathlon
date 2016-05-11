5937fff54c0bc1785fc44273acd6d11785573819
A. Zolliker <andreas.zolliker@hsr.ch>
2016-05-11 15:01:26 +0200

    Extract usb-serial-logic to ConnectionManager

--- ZEDControl/app/src/main/java/ch/hsr/zedcontrol/MainActivity.java
+++ ZEDControl/app/src/main/java/ch/hsr/zedcontrol/MainActivity.java
@@ -1,24 +1,16 @@
 package ch.hsr.zedcontrol;
 
-import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.hardware.usb.UsbDevice;
-import android.hardware.usb.UsbDeviceConnection;
 import android.hardware.usb.UsbManager;
 import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
-
-import com.felhr.usbserial.UsbSerialDevice;
-import com.felhr.usbserial.UsbSerialInterface;
-
-import java.io.UnsupportedEncodingException;
-import java.util.HashMap;
 
 import ch.hsr.zedcontrol.connect.ConnectionManager;
 
@@ -29,97 +21,35 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
-    private static final String ACTION_USB_PERMISSION = ".ACTION_USB_PERMISSION";
-    private static final int VENDOR_ID_FTDI_USB_TO_SERIAL_CABLE = 1027;
-
     private View _contentView;
 
     private ConnectionManager _connectionManager;
-    private UsbManager _usbManager;
-    private UsbDevice _usbDevice;
-
-    private final BroadcastReceiver _usbActionReceiver = new BroadcastReceiver() {
-
-        private UsbSerialDevice serialPort;
-
+    private BroadcastReceiver _connectionReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
             switch (intent.getAction()) {
-                case ACTION_USB_PERMISSION:
-                    handleActionUsbPermission(intent);
+                case ConnectionManager.ACTION_USB_PERMISSION:
+                    Boolean success = intent.getBooleanExtra(ConnectionManager.EXTRA_USB_PERMISSION_SUCCESS, false);
+                    if (success) {
+                        Toast.makeText(getApplicationContext(), "Connected", Toast.LENGTH_LONG).show();
+                    } else {
+                        String error = intent.getStringExtra(ConnectionManager.EXTRA_USB_PERMISSION_ERROR);
+                        if (error != null) {
+                            //FIXME: consider using an error dialog
+                            Toast.makeText(getApplicationContext(), error, Toast.LENGTH_LONG).show();
+                        }
+                    }
                     break;
 
                 case UsbManager.ACTION_USB_DEVICE_ATTACHED:
-                    Log.i(TAG, "onReceive() -> ACTION_USB_DEVICE_ATTACHED");
-                    initUsbDevices();
+                    Log.i(TAG, "_usbActionReceiver.onReceive() -> ACTION_USB_DEVICE_ATTACHED");
+                    Toast.makeText(getApplicationContext(), "Device attached", Toast.LENGTH_SHORT).show();
                     break;
 
                 case UsbManager.ACTION_USB_DEVICE_DETACHED:
-                    Log.i(TAG, "onReceive() -> ACTION_USB_DEVICE_DETACHED");
+                    Log.i(TAG, "_usbActionReceiver.onReceive() -> ACTION_USB_DEVICE_DETACHED");
                     Toast.makeText(getApplicationContext(), "Device detached", Toast.LENGTH_LONG).show();
-                    try {
-                        serialPort.close();
-                    } catch (NullPointerException e) {
-                        // ignore
-                    }
                     break;
-            }
-        }
-
-        private void handleActionUsbPermission(Intent intent) {
-            boolean granted = intent.getExtras().getBoolean(UsbManager.EXTRA_PERMISSION_GRANTED);
-            if (!granted) {
-                Log.w(TAG, "Permission not granted.");
-                Toast.makeText(getApplicationContext(), "No Permission (UsbManager)", Toast.LENGTH_LONG).show();
-                return;
-            }
-
-            UsbDeviceConnection connection = _usbManager.openDevice(_usbDevice);
-            if (connection == null) {
-                Log.e(TAG, "onReceive() -> connection is null - _usbManager.openDevice() FAILED.");
-                return;
-            }
-
-            serialPort = UsbSerialDevice.createUsbSerialDevice(_usbDevice, connection);
-            if (serialPort == null) {
-                Log.e(TAG, "Port is null.");
-                Toast.makeText(getApplicationContext(), "serialPort create error", Toast.LENGTH_LONG).show();
-                return;
-            }
-
-            if (serialPort.open()) {
-                initSerialPort();
-                Log.i(TAG, "_usbActionReceiver.onReceive() -> serialPort open!");
-                Toast.makeText(getApplicationContext(), "serialPort open", Toast.LENGTH_LONG).show();
-
-            } else {
-                Log.e(TAG, "serialPort could not be opened.");
-                Toast.makeText(getApplicationContext(), "serialPort open error", Toast.LENGTH_LONG).show();
-            }
-        }
-
-        private void initSerialPort() {
-            serialPort.setBaudRate(38400);
-            serialPort.setDataBits(UsbSerialInterface.DATA_BITS_8);
-            serialPort.setParity(UsbSerialInterface.PARITY_NONE);
-            serialPort.setStopBits(UsbSerialInterface.STOP_BITS_1);
-            serialPort.setFlowControl(UsbSerialInterface.FLOW_CONTROL_OFF);
-            serialPort.read(_usbReadCallback);
-        }
-    };
-
-
-    UsbSerialInterface.UsbReadCallback _usbReadCallback = new UsbSerialInterface.UsbReadCallback() {
-        @Override
-        public void onReceivedData(byte[] arg0) {
-            String data;
-            try {
-                data = new String(arg0, "UTF-8");
-                data.concat("/n");
-                //TODO: do something with data
-                Toast.makeText(getApplicationContext(), "UsbReadCallback data: " + data, Toast.LENGTH_LONG).show();
-            } catch (UnsupportedEncodingException e) {
-                e.printStackTrace();
             }
         }
     };
@@ -137,17 +67,18 @@ public class MainActivity extends AppCompatActivity {
                 .add(R.id.fragment_container, new MainFragment())
                 .commit();
 
-        _usbManager = (UsbManager) getSystemService(USB_SERVICE);
-        registerBroadcastReceiver();
+        initConnectionManager();
     }
 
 
-    private void registerBroadcastReceiver() {
-        IntentFilter filter = new IntentFilter(ACTION_USB_PERMISSION);
+    private void initConnectionManager() {
+        _connectionManager = new ConnectionManager(this);
+
+        IntentFilter filter = new IntentFilter(ConnectionManager.ACTION_USB_PERMISSION);
         filter.addAction(UsbManager.ACTION_USB_DEVICE_ATTACHED);
         filter.addAction(UsbManager.ACTION_USB_DEVICE_DETACHED);
 
-        registerReceiver(_usbActionReceiver, filter);
+        LocalBroadcastManager.getInstance(this).registerReceiver(_connectionReceiver, filter);
     }
 
 
@@ -172,30 +103,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    private void initUsbDevices() {
-        HashMap<String, UsbDevice> usbDevices = _usbManager.getDeviceList();
-
-        for (UsbDevice device : usbDevices.values()) {
-
-            if (device.getVendorId() == VENDOR_ID_FTDI_USB_TO_SERIAL_CABLE) {
-                _usbDevice = device;
-                PendingIntent pi = PendingIntent.getBroadcast(this, 0, new Intent(ACTION_USB_PERMISSION), 0);
-                _usbManager.requestPermission(device, pi);
-                break;
-            }
-
-            Log.w(TAG, "Unknown vendorID: " + device.getVendorId());
-            Log.w(TAG, "Unknown ManufacturerName: " + device.getManufacturerName());
-            Log.w(TAG, "Unknown DeviceName: " + device.getDeviceName());
-            Log.w(TAG, "Unknown ProductName: " + device.getProductName());
-            Toast.makeText(this, "Unknown vendorId: " + device.getVendorId(), Toast.LENGTH_LONG).show();
-        }
-    }
-
-
     @Override
     protected void onDestroy() {
-        unregisterReceiver(_usbActionReceiver);
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(_connectionReceiver);
+        _connectionManager.dispose(this);
 
         super.onDestroy();
     }
--- ZEDControl/app/src/main/java/ch/hsr/zedcontrol/connect/ConnectionManager.java
+++ ZEDControl/app/src/main/java/ch/hsr/zedcontrol/connect/ConnectionManager.java
@@ -1,9 +1,194 @@
 package ch.hsr.zedcontrol.connect;
 
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbDeviceConnection;
+import android.hardware.usb.UsbManager;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.felhr.usbserial.UsbSerialDevice;
+import com.felhr.usbserial.UsbSerialInterface;
+
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+
 /**
  * Handles the connection to the peripheral (roboRIO)
  */
 public class ConnectionManager {
+    public static final String ACTION_USB_PERMISSION = ".ACTION_USB_PERMISSION";
+    public static final String EXTRA_USB_PERMISSION_ERROR = "/EXTRA_USB_PERMISSION_ERROR";
+    public static final String EXTRA_USB_PERMISSION_SUCCESS = "/EXTRA_USB_PERMISSION_SUCCESS";
 
+    private static String TAG = ConnectionManager.class.getSimpleName();
+
+    private static final int VENDOR_ID_FTDI_USB_TO_SERIAL_CABLE = 1027;
+
+    private UsbManager _usbManager;
+    private UsbDevice _usbDevice;
+    private UsbSerialDevice _serialPort;
+
+    //TODO: check if it is possible to get rid of storing Context as a field (always pass as parameter)
+    private Context _context;
+
+    private final BroadcastReceiver _usbActionReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            switch (intent.getAction()) {
+                case ACTION_USB_PERMISSION:
+                    Log.i(TAG, "_usbActionReceiver.onReceive() -> ACTION_USB_PERMISSION");
+                    handleActionUsbPermission(intent);
+                    break;
+
+                case UsbManager.ACTION_USB_DEVICE_ATTACHED:
+                    Log.i(TAG, "_usbActionReceiver.onReceive() -> ACTION_USB_DEVICE_ATTACHED");
+                    initUsbDevices();
+                    LocalBroadcastManager.getInstance(_context).sendBroadcast(intent);
+                    break;
+
+                case UsbManager.ACTION_USB_DEVICE_DETACHED:
+                    Log.i(TAG, "_usbActionReceiver.onReceive() -> ACTION_USB_DEVICE_DETACHED");
+                    closeSerialPort();
+                    LocalBroadcastManager.getInstance(_context).sendBroadcast(intent);
+                    break;
+            }
+        }
+
+        private void handleActionUsbPermission(Intent intent) {
+            boolean granted = intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false);
+            if (!granted) {
+                Log.w(TAG, "handleActionUsbPermission() -> Permission not granted.");
+                intent.putExtra(EXTRA_USB_PERMISSION_SUCCESS, false);
+                intent.putExtra(EXTRA_USB_PERMISSION_ERROR, "Permission not granted.");
+                LocalBroadcastManager.getInstance(_context).sendBroadcast(intent);
+                return;
+            }
+
+            UsbDeviceConnection connection = _usbManager.openDevice(_usbDevice);
+            if (connection == null) {
+                Log.e(TAG, "handleActionUsbPermission() -> connection is null - _usbManager.openDevice() FAILED.");
+                intent.putExtra(EXTRA_USB_PERMISSION_SUCCESS, false);
+                intent.putExtra(EXTRA_USB_PERMISSION_ERROR, "Could not establish connection.");
+                LocalBroadcastManager.getInstance(_context).sendBroadcast(intent);
+                return;
+            }
+
+            _serialPort = UsbSerialDevice.createUsbSerialDevice(_usbDevice, connection);
+            if (_serialPort == null) {
+                Log.e(TAG, "handleActionUsbPermission() -> _serialPort is null.");
+                intent.putExtra(EXTRA_USB_PERMISSION_SUCCESS, false);
+                intent.putExtra(EXTRA_USB_PERMISSION_ERROR, "Could not create usb serial device.");
+                LocalBroadcastManager.getInstance(_context).sendBroadcast(intent);
+                return;
+            }
+
+            if (_serialPort.open()) {
+                initSerialPort();
+                Log.i(TAG, "handleActionUsbPermission() -> _serialPort open!");
+                intent.putExtra(EXTRA_USB_PERMISSION_SUCCESS, true);
+                LocalBroadcastManager.getInstance(_context).sendBroadcast(intent);
+            } else {
+                Log.e(TAG, "handleActionUsbPermission() -> _serialPort could not be opened.");
+                intent.putExtra(EXTRA_USB_PERMISSION_SUCCESS, false);
+                intent.putExtra(EXTRA_USB_PERMISSION_ERROR, "Serial port could not be opened.");
+                LocalBroadcastManager.getInstance(_context).sendBroadcast(intent);
+            }
+        }
+
+    };
+
+
+    private void initSerialPort() {
+        _serialPort.setBaudRate(38400);
+        _serialPort.setDataBits(UsbSerialInterface.DATA_BITS_8);
+        _serialPort.setParity(UsbSerialInterface.PARITY_NONE);
+        _serialPort.setStopBits(UsbSerialInterface.STOP_BITS_1);
+        _serialPort.setFlowControl(UsbSerialInterface.FLOW_CONTROL_OFF);
+        _serialPort.read(_usbReadCallback);
+    }
+
+
+    private void closeSerialPort() {
+        try {
+            _serialPort.close();
+        } catch (NullPointerException e) {
+            Log.w(TAG, "closeSerialPort() -> catch NullPointerException, was the port open already?");
+        }
+    }
+
+
+    private void initUsbDevices() {
+        HashMap<String, UsbDevice> usbDevices = _usbManager.getDeviceList();
+
+        for (UsbDevice device : usbDevices.values()) {
+
+            if (device.getVendorId() == VENDOR_ID_FTDI_USB_TO_SERIAL_CABLE) {
+                _usbDevice = device;
+                PendingIntent pi = PendingIntent.getBroadcast(_context, 0, new Intent(ACTION_USB_PERMISSION), 0);
+                _usbManager.requestPermission(device, pi);
+                break;
+            }
+
+            Log.w(TAG, "Unknown vendorID: " + device.getVendorId());
+            Log.w(TAG, "Unknown ManufacturerName: " + device.getManufacturerName());
+            Log.w(TAG, "Unknown DeviceName: " + device.getDeviceName());
+            Log.w(TAG, "Unknown ProductName: " + device.getProductName());
+        }
+    }
+
+
+    UsbSerialInterface.UsbReadCallback _usbReadCallback = new UsbSerialInterface.UsbReadCallback() {
+        @Override
+        public void onReceivedData(byte[] arg0) {
+            String data;
+            try {
+                data = new String(arg0, "UTF-8");
+                data.concat("/n");
+                //TODO: do something with data
+                Log.i(TAG, "_usbReadCallback.onReceivedData: " + data);
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+        }
+    };
+
+
+    /**
+     * Constructor for ConnectionManager needs a Context to be able to use Android methods.
+     *
+     * @param context The Context object, usually an Activity.
+     */
+    public ConnectionManager(Context context) {
+        _context = context;
+        _usbManager = (UsbManager) context.getSystemService(Context.USB_SERVICE);
+
+        registerBroadcastReceiver(context);
+    }
+
+
+    private void registerBroadcastReceiver(Context context) {
+        IntentFilter filter = new IntentFilter(ACTION_USB_PERMISSION);
+        filter.addAction(UsbManager.ACTION_USB_DEVICE_ATTACHED);
+        filter.addAction(UsbManager.ACTION_USB_DEVICE_DETACHED);
+
+        context.registerReceiver(_usbActionReceiver, filter);
+    }
+
+
+    /**
+     * Closes all open connections and unregisters the receivers.
+     *
+     * @param context
+     */
+    public void dispose(Context context) {
+        context.unregisterReceiver(_usbActionReceiver);
+        closeSerialPort();
+    }
 
 }
