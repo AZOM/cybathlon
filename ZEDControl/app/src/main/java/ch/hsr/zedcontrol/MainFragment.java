a54c1c92b60a4a4c05a54d0a4b21963fe5725767
A. Zolliker <andreas.zolliker@hsr.ch>
2016-05-23 16:58:11 +0200

    Visualize active drive mode in MainFragment

--- ZEDControl/app/src/main/java/ch/hsr/zedcontrol/MainFragment.java
+++ ZEDControl/app/src/main/java/ch/hsr/zedcontrol/MainFragment.java
@@ -8,6 +8,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.RadioGroup;
+import android.widget.ToggleButton;
 
 import ch.hsr.zedcontrol.roborio.ConnectionManager;
 import ch.hsr.zedcontrol.roborio.RoboRIOModes;
@@ -21,12 +23,17 @@ public class MainFragment extends Fragment {
 
     private ConnectionManager _connectionManager;
 
+    private ToggleButton _buttonDefaultDriveMode;
+
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_main, container, false);
 
         initButtons(view);
+
+        _buttonDefaultDriveMode = (ToggleButton) view.findViewById(R.id.button_mode_none);
 
         return view;
     }
@@ -35,9 +42,9 @@ public class MainFragment extends Fragment {
     private void initButtons(View view) {
         initButtonPowerOff(view);
         initButtonStartUp(view);
-        initButtonModeFreeDriving(view);
-        initButtonModeNone(view);
         initButtonModeStairs(view);
+
+        initButtonsDriveModes(view);
     }
 
 
@@ -65,30 +72,6 @@ public class MainFragment extends Fragment {
     }
 
 
-    private void initButtonModeFreeDriving(View view) {
-        Button button = (Button) view.findViewById(R.id.button_mode_free_driving);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.i(TAG, "Requesting mode: DRIVE_FREE");
-                _connectionManager.requestMode(RoboRIOModes.DRIVE_FREE);
-            }
-        });
-    }
-
-
-    private void initButtonModeNone(View view) {
-        Button button = (Button) view.findViewById(R.id.button_mode_none);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Log.i(TAG, "Requesting mode: NO_MODE");
-                _connectionManager.requestMode(RoboRIOModes.NO_MODE);
-            }
-        });
-    }
-
-
     private void initButtonModeStairs(View view) {
         Button button = (Button) view.findViewById(R.id.button_driving_stairs);
         button.setOnClickListener(new View.OnClickListener() {
@@ -105,11 +88,69 @@ public class MainFragment extends Fragment {
     }
 
 
+    private void initButtonsDriveModes(View view) {
+        initToggleButtonModeFreeDriving(view);
+        initToggleButtonModeNone(view);
+        initRadioGroup(view);
+    }
+
+
+    private void initToggleButtonModeFreeDriving(View view) {
+        final ToggleButton button = (ToggleButton) view.findViewById(R.id.button_mode_free_driving);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "Requesting mode: DRIVE_FREE");
+                _connectionManager.requestMode(RoboRIOModes.DRIVE_FREE);
+
+                markAsCheckedWithinRadioGroup(v);
+            }
+        });
+    }
+
+
+    private void markAsCheckedWithinRadioGroup(View v) {
+        // let the ToggleButton behave like a RadioButton -> only one can be active at a time
+        ((RadioGroup) v.getParent()).check(0);
+        ((RadioGroup) v.getParent()).check(v.getId());
+    }
+
+
+    private void initToggleButtonModeNone(View view) {
+        final ToggleButton button = (ToggleButton) view.findViewById(R.id.button_mode_none);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "Requesting mode: NO_MODE");
+                _connectionManager.requestMode(RoboRIOModes.NO_MODE);
+
+                markAsCheckedWithinRadioGroup(v);
+            }
+        });
+    }
+
+
+    private void initRadioGroup(View view) {
+        ((RadioGroup) view.findViewById(R.id.group_drive_mode)).setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                for (int index = 0; index < group.getChildCount(); index++) {
+                    final ToggleButton b = (ToggleButton) group.getChildAt(index);
+                    b.setChecked(b.getId() == checkedId);
+                }
+            }
+        });
+    }
+
+
     @Override
     public void onResume() {
         super.onResume();
         // obtain instance with current state from parent activity
         _connectionManager = ((MainActivity) getActivity()).connectionManager;
+
+        _buttonDefaultDriveMode.performClick();
     }
+
 
 }
